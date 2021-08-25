870bc1992ca056a0d2155b76a1e78f8c617cb390
Rieper <>
2021-08-25 18:24:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function getWeather (country,weatherType){
+return 'The weather in ' + country + ' is ' + weatherType +'';
+}
+
+console.log (getWeather('Scotland','sunny'));
+
+console.log (getWeather('Portugal','wonderful'));
+
+console.log (getWeather('Florida','sweaty'));
